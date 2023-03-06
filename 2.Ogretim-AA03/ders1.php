48c5d73acf6f024f0cd7d0ee65e87418ea3b67bd
Mehmet Can Hanaylı <mehmetcanhanayli@gmail.com>
2023-03-06 21:42:26 +0300

    2. Öğretim ilk dersi tamamlandı.

---
+++ 2.Ogretim-AA03/ders1.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Php Ders - 1 </title>
+</head>
+
+<body>
+    <?php
+    echo 'Merhaba';
+    echo "<br>";
+    echo "<hr><h3>Merhaba</h3>";
+    echo "<hr><h1>Nasılsınız ...</h1>";
+    echo "<hr><h6>Nasılsınız ...</h6>";
+    echo "<hr><h2>Nasılsınız ...</h2>";
+    echo "<hr><h4>Nasılsınız ...</h4>";
+
+    ?>
+</body>
+
+</html>
